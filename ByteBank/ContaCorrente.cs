362e17b85f47d193416b644abef1d7e88e6fd7a1
Heidson96 <heidson96@gmail.com>
2021-10-15 22:33:43 -0300

    Initial commit

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public string Titular { get; set; }
+        public int Conta { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; set; }
+        
+        public double Taxa { get; set; } = 2;
+
+        public ContaCorrente()
+        {
+            
+        }
+        
+        public ContaCorrente(string titular, int conta, int agencia, double saldo)
+        {
+            Titular = titular;
+            Conta = conta;
+            Agencia = agencia;
+            Saldo = saldo = 5;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo -= valor + Taxa;
+        }
+
+        public override string ToString()
+        {
+            return "Titular: " + Titular + ", " +
+                   "Agência: " + Agencia + ", " +
+                   "Conta Corrente: " + Conta + ", " +
+                   "Saldo: R$ " + Saldo;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente("Glória", 8364, 866487, 1000.0);
+            ContaCorrente c2 = new ContaCorrente("Heidson", 0760, 176347, 500.0);
+            ContaCorrente c3 = new ContaCorrente("José", 0760, 175557, 0);
+            
+            c2.Depositar(500.0);
+            c2.Sacar(400.0);
+            c3.Sacar(3);
+            
+            Console.WriteLine("O saldo atualizado em conta é: " + c2);
+            Console.WriteLine("O saldo atualizado em conta é: " + c3);
+
+        }
+    }
+}
